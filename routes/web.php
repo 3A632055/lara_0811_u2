ee14362141fddde9c71c6458a04085bfd72b800c
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2019-10-21 18:01:27 +0800

    <修改> 單元練習< 練習1-1>

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,11 @@ Route::get('/', function () {
 });
 */
 
+// 練習 1：設定 Route 反應
 //1. 設定 Route 回傳字串
-/*
+
 Route::get('/', function() {
   return 'welcome';
 });
-*/
 
-//2. 設定 Route 回傳 view
-/*
-Route::get('/', function() {
-    return view('welcome');
-});
-*/
 
-//3. 設定 Route 跳轉頁面
- Route::get('/', function() {
-    return redirect('welcome');
-});

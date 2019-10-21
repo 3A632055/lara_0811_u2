031925a30711873d29779d3bc5aa5c9427ec01d8
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2019-10-21 17:48:02 +0800

    單元練習< 練習一>

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+//1. 設定 Route 回傳字串
+/*
+Route::get('/', function() {
+  return 'welcome';
+});
+*/
+
+//2. 設定 Route 回傳 view
+/*
+Route::get('/', function() {
+    return view('welcome');
+});
+*/
+
+//3. 設定 Route 跳轉頁面
+ Route::get('/', function() {
+    return redirect('welcome');
 });

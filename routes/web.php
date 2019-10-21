1f58932e03540a0875d01e200e4c828e7ca90ece
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2019-10-21 21:42:42 +0800

    單元練習< 練習7-1>

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 /*
 Route::get('/', function () {
     return view('welcome');
@@ -88,8 +89,16 @@ Route::get('dashboard',function(){
 */
 
 //2. 設定另一個 Route 以群組包起來設定 prefix
+/*
 Route::group(['prefix'=>'admin'],function(){
     Route::get('dashboard',function(){
         return 'admin dashboard';
     });
 });
+*/
+
+//練習 7：將 Route 的內容搬至 Controller 內
+//1. 編輯 app/Http/routes.php如：
+Route::get('/', ['as' => 'home.index', 'uses' =>
+    'HomeController@index']);
+

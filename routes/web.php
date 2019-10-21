48dedd4d10c72507ca8ada2ff594eb777c4bc59c
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2019-10-21 18:03:30 +0800

    單元練習< 練習1-3>

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,15 @@ Route::get('/', function() {
 */
 
 //2. 設定 Route 回傳 view
-
+/*
 Route::get('/', function() {
     return view('welcome');
 });
+*/
+
+//3. 設定 Route 跳轉頁面
+
+ Route::get('/', function() {
+    return redirect('welcome');
+});
+
